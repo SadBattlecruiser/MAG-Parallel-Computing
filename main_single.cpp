97e6b7b42ca4ac0e864b00818a7a6206e9b07299
Rashid Abidokov <N23R35@gmail.com>
2020-09-26 17:31:32 +0300

    Конструктор сетки

--- main_single.cpp
+++ main_single.cpp
@@ -12,6 +12,13 @@ public:
   double y;
 };
 
+// Та же точка, но с температурой
+class Point_T : public Point {
+public:
+  Point_T(double x0=0, double y0=0, double T0=0);
+  double T;
+};
+
 // Класс рамки - просто форма разбиваемой области
 // Прямоугольник с прямоугольным вырезом внутри
 class Frame {
@@ -28,22 +35,40 @@ public:
   Point get_bi() const;
   Point get_ci() const;
   Point get_di() const;
+  vector<double> get_side_len() const;
   void print() const;
 private:
+  vector<double> side_len_;
   // Точки углов с левого нижнего против часовой
-  Point a_o, b_o, c_o, d_o; // Без _ ругается на do
-  Point a_i, b_i, c_i, d_i;
+  Point ao_, bo_, co_, do_;
+  Point ai_, bi_, ci_, di_;
 };
 
 
+// Класс сетки
+class Mesh {
+public:
+  // Подразумевается, что стороны выреза нацело делятся на получающиеся шаги сетки. Иначе бобо
+  Mesh(const Frame& frame, const unsigned N_x, const unsigned N_y, const double T0 = 0, const bool Symmetry = true);
+private:
+  bool sym_;
+  double x_step_;
+  double y_step_;
+  Point_T** mesh_;
+  unsigned N_xo_; // кол-во узлов в бОльшей части рамки - где нет выреза
+  unsigned N_xi_; // кол-во узлов в меньшей части рамки
+  unsigned N_yo_;
+  unsigned N_yi_;
+};
+
 int main() {
   cout << "Hello, World!" << endl;
-  Point tp(1);
+  Point_T tp(1, 2, 30);
   //tp.print();
   double side_len[] = {6., 8., 2., 4.};
   Frame tf(side_len);
   tf.print();
-
+  Mesh ms(tf, 8, 6);
   return 0;
 };
 
@@ -56,84 +81,130 @@ Point::Point(double x0, double y0) {
 
 void Point::print() const {
   cout << "x: " << x << " y: " << y;
-}
+};
+
+
+Point_T::Point_T(double x0, double y0, double T0) :
+  Point(x0, y0) {
+    T = T0;
+  };
 
 
 Frame::Frame(const double* side_len) {
-  a_o = Point(0, 0);
-  b_o = Point(0, side_len[0]);
-  c_o = Point(side_len[1], side_len[0]);
-  d_o = Point(side_len[1], 0);
-  double a_ix = (side_len[1] - side_len[3]) / 2;
-  double a_iy = (side_len[0] - side_len[2]) / 2;
-  a_i = Point(a_ix, a_iy);
-  b_i = Point(a_ix, a_iy + side_len[2]);
-  c_i = Point(a_ix + side_len[3], a_iy + side_len[2]);
-  d_i = Point(a_ix + side_len[3], a_iy);
+  side_len_ = vector<double>(side_len, side_len + 4);
+  ao_ = Point(0, 0);
+  bo_ = Point(0, side_len_[0]);
+  co_ = Point(side_len_[1], side_len_[0]);
+  do_ = Point(side_len_[1], 0);
+  double a_ix = (side_len_[1] - side_len_[3]) / 2;
+  double a_iy = (side_len_[0] - side_len_[2]) / 2;
+  ai_ = Point(a_ix, a_iy);
+  bi_ = Point(a_ix, a_iy + side_len_[2]);
+  ci_ = Point(a_ix + side_len_[3], a_iy + side_len_[2]);
+  di_ = Point(a_ix + side_len_[3], a_iy);
 };
 
 Point Frame::get_ao() const {
-  return a_o;
+  return ao_;
 }
 
 Point Frame::get_bo() const {
-  return b_o;
+  return bo_;
 }
 
 Point Frame::get_co() const {
-  return c_o;
+  return co_;
 }
 
 Point Frame::get_do() const {
-  return d_o;
+  return do_;
 }
 
 Point Frame::get_ai() const {
-  return a_i;
+  return ai_;
 }
 
 Point Frame::get_bi() const {
-  return b_i;
+  return bi_;
 }
 
 Point Frame::get_ci() const {
-  return c_i;
+  return ci_;
 }
 
 Point Frame::get_di() const {
-  return d_i;
+  return di_;
 }
+
+vector<double> Frame::get_side_len() const {
+  return side_len_;
+};
 
 void Frame::print() const {
   cout << "ao: " << endl;
-  a_o.print();
+  ao_.print();
   cout << endl;
 
   cout << "bo: " << endl;
-  b_o.print();
+  bo_.print();
   cout << endl;
 
   cout << "co: " << endl;
-  c_o.print();
+  co_.print();
   cout << endl;
 
   cout << "do: " << endl;
-  d_o.print();
+  do_.print();
   cout << endl;
 
   cout << "ai: " << endl;
-  a_i.print();
+  ai_.print();
   cout << endl;
 
   cout << "bi: " << endl;
-  b_i.print();
+  bi_.print();
   cout << endl;
 
   cout << "ci: " << endl;
-  c_i.print();
+  ci_.print();
   cout << endl;
 
   cout << "di: " << endl;
-  d_i.print();
+  di_.print();
   cout << endl;
 }
+
+
+Mesh::Mesh(const Frame& frame, const unsigned N_x, const unsigned N_y, const double T0, const bool Symmetry) {
+  if (Symmetry) {
+    vector<double> side_len = frame.get_side_len();
+    sym_ = true;
+
+    N_xo_ = N_x;
+    x_step_ = side_len[0] / N_xo_;
+    N_yo_ = N_y;
+    y_step_ = side_len[1] / N_yo_;
+    N_xi_ = static_cast<unsigned>(side_len[2] / x_step_);
+    N_yi_ = static_cast<unsigned>(side_len[3] / y_step_);
+
+    mesh_ = new Point_T*[N_xo_];
+    // Сначала длинные массивы - не упирающиеся в вырез
+    for (int i = 0; i < N_yi_; ++i) {
+      mesh_[i] = new Point_T[N_xo_];
+      for (int j = 0; j < N_xo_; ++j) {
+        mesh_[i][j] = Point_T(x_step_ * j, y_step_ * i, T0);
+      }
+    }
+    // Дальше короткие - которые упираются в вырез
+    for (int i = N_yi_; i < N_yo_; ++i) {
+      mesh_[i] = new Point_T[N_xi_];
+      for (int j = 0; j < N_xi_; ++j) {
+        mesh_[i][j] = Point_T(x_step_ * j, y_step_ * i, T0);
+      }
+    }
+
+    //cout << "Mesh initializing" << endl;
+  }
+  else
+    throw "symmetry=false in Mesh";
+};
